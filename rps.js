2930c547b23972b2d093faa2b6fbf69d32fe7aa5
DaymionS <ionis.online@gmail.com>
2025-05-18 15:23:52 -0500

    Add a single round of the game
    A function takes the inputs and decides the winner. The score is incremented and displayed. There were some name changes to variables inside functions

--- rps.js
+++ rps.js
@@ -7,8 +7,68 @@ function GetComputerChoice() {
 //console.log(GetComputerChoice());
 
 function GetHumanChoice() {
-    let humanChoice = prompt("Choose Rock, Paper, or Scissors");
-    humanChoice = humanChoice.toLocaleLowerCase();
-    return humanChoice;
+    let playerChoice = prompt("Choose Rock, Paper, or Scissors");
+    playerChoice = playerChoice.toLocaleLowerCase();
+    return playerChoice;
 }
 //console.log(GetHumanChoice());
+
+let humanScore = 0; 
+let computerScore = 0;
+//console.log(humanScore, computerScore);
+
+function playRound(humanChoice, computerChoice) {
+    switch(computerChoice) {
+        case 0:
+            switch(humanChoice) {
+                case "rock":
+                    console.log("Draw! Rock equals Rock!");
+                    break;
+                case "paper":
+                    console.log("You Win! Paper beats Rock!");
+                    humanScore++;
+                    break;
+                case "scissors":
+                    console.log("You Lose! Rock beats Scissors!");
+                    computerScore++;
+                    break;
+            }
+            break;
+        case 1:
+            switch(humanChoice) {
+                case "rock":
+                    console.log("You Lose! Paper beats Rock!");
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log("Draw! Paper equals Paper!");
+                    break;
+                case "scissors":
+                    console.log("You Win! Scissors beats Paper!");
+                    humanScore++;
+                    break;
+            }
+            break;
+        case 2:
+            switch(humanChoice) {
+                case "rock":
+                    console.log("You Win! Rock beats Scissors!");
+                    humanScore++;
+                    break;
+                case "paper":
+                    console.log("You Lose! Scissors beats Paper!");
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log("Draw! Scissors equals Scissors");
+                    break;
+            }
+            break;
+    }
+    console.log(`Player: ${humanScore} CPU: ${computerScore}`);
+}
+
+const humanSelection = GetHumanChoice();
+const computerSelection = GetComputerChoice();
+
+playRound(humanSelection, computerSelection);
