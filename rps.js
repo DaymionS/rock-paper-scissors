39febb4df51f770915549a52246185c83ecadad8
DaymionS <ionis.online@gmail.com>
2025-05-18 14:54:15 -0500

    Create function to get the computer choice
    A function returns one of three values that will be used to determine if the computer chooses rock paper or scissors.

--- rps.js
+++ rps.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+//console.log("Hello World!");
+
+function GetComputerChoice() {
+    let cpuChoice = Math.floor((Math.random() * 100)) % 3;
+    return cpuChoice; // Returns 0, 1, or 2
+}
+//console.log(GetComputerChoice());
